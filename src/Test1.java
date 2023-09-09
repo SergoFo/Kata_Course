53ac86d12a2f455f28dd0556177d15ef372750d3
Никита <nikita-vip1904@yandex.ru>
2023-09-09 13:37:19 +0300

    add new method chekOstatok

--- src/Test1.java
+++ src/Test1.java
@@ -2,7 +2,12 @@ public class Test1 {
     public static void main(String[] args) {
         double b = 5;
         int c = 5;
-        double a = b%c;
+        double a = b % c;
         System.out.println(a);
+        System.out.println(chekOstatok(5,5));
+    }
+
+    public static double chekOstatok(double a, int b) {
+        return a % b;
     }
 }
