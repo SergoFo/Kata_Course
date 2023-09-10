dc432cbf60824f0bd979bb3f32019773a354871b
Sergo Fan <sergei.fonarev@yandex.ru>
2023-09-10 16:58:32 +0300

    add

--- src/Test_BigInteger.java
+++ src/Test_BigInteger.java
@@ -3,7 +3,10 @@ import java.math.BigInteger;
 public class Test_BigInteger {
 
     public static void main(String[] args) {
+        maxLongSqr();
+    }
 
+    public static void maxLongSqr() {
         BigInteger maxLong = BigInteger.valueOf(Long.MAX_VALUE);
         BigInteger square = maxLong.multiply(maxLong);
         System.out.println(square);
