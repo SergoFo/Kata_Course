d38bb5c92b6133e4b6139d7a64ab99dba99812cd
Sergo Fan <sergei.fonarev@yandex.ru>
2023-09-19 02:00:38 +0300

    add

--- src/BigInteger_factorial_1_4_7.java
+++ src/BigInteger_factorial_1_4_7.java
@@ -1,9 +1,9 @@
 import java.math.BigInteger;
 
 public class BigInteger_factorial_1_4_7 {
-    public static BigInteger factorial(int value){
+    public static BigInteger factorial(int value) {
         BigInteger one = BigInteger.ONE;
-        for (int i = 1; i <= value; i++){
+        for (int i = 1; i <= value; i++) {
             one = one.multiply(BigInteger.valueOf(i));
         }
         return one;
