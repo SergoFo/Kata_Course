5c61254ead0ec1b8c5b5a825ed3529086c52cac3
Sergo Fan <sergei.fonarev@yandex.ru>
2023-09-21 13:10:13 +0300

    add

--- src/getArrayMiddle_1_5_5.java
+++ src/getArrayMiddle_1_5_5.java
@@ -1,14 +1,21 @@
 public class getArrayMiddle_1_5_5 {
     public static int[] getArrayMiddle(int[] numbers) {
-        int elementsCount = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers.length % 2 == 0) {
-                elementsCount++;
+        if (numbers.length > 0) {
+            System.out.print("[");
+            if (numbers.length % 2 == 0) {                           //Четные
+                int a = numbers[numbers.length / 2];
+                int b = numbers[numbers.length / 2 - 1];
+                System.out.println(b + ", " + a + "]");
+                return new int[]{b, a};
             } else {
-                return numbers.length / 2;
+                int c = numbers[numbers.length / 2];                 //Нечетные
+                System.out.println(c + "]");
+                return new int[]{c};
             }
+        } else {
+            System.out.println("[]");
+            return new int[0];
         }
-        System.out.println(elementsCount);
-        return numbers;
     }
 }
+
