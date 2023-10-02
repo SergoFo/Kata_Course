6a4337a360cbe066311eb8ef720e13418074a6ac
Sergo Fan <sergei.fonarev@yandex.ru>
2023-10-02 18:16:50 +0300

    add

---
+++ modul_2/pz_2_1_6.java
@@ -0,0 +1,25 @@
+public class pz_2_1_6 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        dog.catchCat(cat);
+    }
+
+    public static class Cat {
+        public void sayHello() {
+            System.out.println("Мяу!");
+        }
+    }
+
+    public static class Dog {
+        public void sayHello() {
+            System.out.println("Гав!");
+        }
+
+        public void catchCat(Cat first) {
+            System.out.println("Кошка поймана");
+            sayHello();
+            first.sayHello();
+        }
+    }
+}
