b4ef83f3db6e9508249873bb35d8ad734e589ea4
Sergo Fan <sergei.fonarev@yandex.ru>
2023-10-03 11:23:05 +0300

    add

---
+++ modul_2/pz_2_1_7.java
@@ -0,0 +1,31 @@
+public class pz_2_1_7 {
+    public static void main(String[] args) {
+        Day current = Day.SUNDAY;
+        System.out.println(current.isWeekend());
+    }
+
+    public enum Day {
+        MONDAY("Понедельник"),
+        TUESDAY("Вторник"),
+        WEDNESDAY("Среда"),
+        THURSDAY("Четверг"),
+        FRIDAY("Пятница"),
+        SATURDAY("Суббота"),
+        SUNDAY("Воскресенье");
+        private final String name;
+
+        Day(String name) {
+            this.name = name;
+        }
+
+        public String getRusName() {
+            return name;
+        }
+
+        public boolean isWeekend() {
+            return getRusName().equals("Суббота") || getRusName().equals("Воскресенье");
+        }
+    }
+}
+
+
