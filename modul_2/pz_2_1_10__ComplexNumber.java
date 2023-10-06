c3a54187f89920407769da12933d42fdefe9dbe7
Sergo Fan <sergei.fonarev@yandex.ru>
2023-10-06 14:42:23 +0300

    add

--- modul_2/pz_2_1_10__ComplexNumber.java
+++ modul_2/pz_2_1_10__ComplexNumber.java
@@ -1,3 +1,23 @@
 public class pz_2_1_10__ComplexNumber {
+    public static class ComplexNumber {
+        private double re;
+        private double im;
 
+        public ComplexNumber() {
+        }
+
+        public ComplexNumber(double re, double im) {
+            this.re = re;
+            this.im = im;
+        }
+
+        public double getRe() {
+            return re;
+        }
+
+        public double getIm() {
+            return im;
+        }
+
+    }
 }
