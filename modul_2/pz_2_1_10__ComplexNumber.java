d7fe9797ae1a2b14432a2f1aebd563d792d01fea
Sergo Fan <sergei.fonarev@yandex.ru>
2023-10-11 13:17:44 +0300

    add

--- modul_2/pz_2_1_10__ComplexNumber.java
+++ modul_2/pz_2_1_10__ComplexNumber.java
@@ -1,4 +1,10 @@
 public class pz_2_1_10__ComplexNumber {
+    public static void main(String[] args) {
+        ComplexNumber a = new ComplexNumber(1, 1);
+        ComplexNumber b = new ComplexNumber(1, 1);
+        System.out.println(a.equals(b));
+    }
+
     public static class ComplexNumber {
         private double re;
         private double im;
@@ -19,5 +25,26 @@ public class pz_2_1_10__ComplexNumber {
             return im;
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ComplexNumber that = (ComplexNumber) o;
+            return Double.compare(that.re, re) == 0 && Double.compare(that.im, im) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            int total = 31;
+            long r = Double.doubleToLongBits(re);
+            long i = Double.doubleToLongBits(im);
+            total = total * 31 + (int) (r ^ (r >>> 32));
+            total = total * 31 + (int) (i ^ (i >>> 32));
+            return total;
+        }
     }
 }
