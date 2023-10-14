e36ab74a939e7da5abebd0f41a47f3bbae06fa29
Sergo Fan <sergei.fonarev@yandex.ru>
2023-10-14 23:50:56 +0300

    add

---
+++ modul_2/pz_2_5_6__AsciiCharSequence.java
@@ -0,0 +1,42 @@
+public class pz_2_5_6__AsciiCharSequence {
+    public static void main(String[] args) {
+        byte[] example = {72, 101, 108, 108, 111, 33};
+        AsciiCharSequence answer = new AsciiCharSequence(example);
+        System.out.println("Последовательность - " + answer.toString());//Hello!
+        System.out.println("Размер её - " + answer.length());//6
+        System.out.println("Символ под № 1 - " + answer.charAt(1));//e
+        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5));//ello
+
+//проверка на нарушение инкапсуляции private поля
+        System.out.println(answer.toString());//Hello!
+        example[0] = 74;
+        System.out.println(answer.toString());//Hello
+    }
+    public static class AsciiCharSequence implements CharSequence {
+        byte[] example;
+
+        public AsciiCharSequence(byte[] example) {
+            this.example = example;
+        }
+
+        @Override
+        public int length() {
+            return example.length;
+        }
+
+        @Override
+        public char charAt(int index) {
+            return 0;
+        }
+
+        @Override
+        public CharSequence subSequence(int start, int end) {
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
+}
