0e49b0bd013bf98fa7b7a0f0c0301085e2b1e6cc
Sergo Fan <sergei.fonarev@yandex.ru>
2023-10-24 16:03:08 +0300

    add

--- modul_2/pz_2_5_6__AsciiCharSequence.java
+++ modul_2/pz_2_5_6__AsciiCharSequence.java
@@ -1,16 +1,18 @@
+import java.util.Arrays;
+
 public class pz_2_5_6__AsciiCharSequence {
     public static void main(String[] args) {
         byte[] example = {72, 101, 108, 108, 111, 33};
         AsciiCharSequence answer = new AsciiCharSequence(example);
-        System.out.println("Последовательность - " + answer.toString());//Hello!
-        System.out.println("Размер её - " + answer.length());//6
-        System.out.println("Символ под № 1 - " + answer.charAt(1));//e
-        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5));//ello
+        System.out.println("Последовательность - " + answer.toString()); //Hello!
+        System.out.println("Размер её - " + answer.length()); //6
+        System.out.println("Символ под № 1 - " + answer.charAt(1)); //e
+        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5)); //ello
 
 //проверка на нарушение инкапсуляции private поля
-        System.out.println(answer.toString());//Hello!
+        System.out.println(answer.toString()); //Hello!
         example[0] = 74;
-        System.out.println(answer.toString());//Hello
+        System.out.println(answer.toString()); //Hello
     }
     public static class AsciiCharSequence implements CharSequence {
         byte[] example;
@@ -26,17 +28,23 @@ public class pz_2_5_6__AsciiCharSequence {
 
         @Override
         public char charAt(int index) {
-            return 0;
-        }
+            return (char) (example[index]);
+                }
 
         @Override
         public CharSequence subSequence(int start, int end) {
-            return null;
+            int length = end - start;
+            byte[] bytes = new byte[length];
+            for (int i = 0, j = start; i < length; i++, j++) {
+                bytes[i] = example[j];
+            }
+            return new AsciiCharSequence(bytes);
         }
 
         @Override
         public String toString() {
-            return super.toString();
+            return new String(example);
         }
+
     }
 }
