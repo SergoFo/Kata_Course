91353ff1f382faae0a013e394dd16bfbd3c63344
Sergo Fan <sergei.fonarev@yandex.ru>
2023-12-26 20:41:57 +0300

    add

--- modul_5/pz_5_2_13__symmetricDifference.java
+++ modul_5/pz_5_2_13__symmetricDifference.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,10 +14,17 @@ public class pz_5_2_13__symmetricDifference {
         s2.add(1);
         s2.add(2);
 
-        Set<Integer> result = symmetricDifference(s1,s2);
+        Set<Integer> result = symmetricDifference(s1, s2);
         System.out.println(result);
     }
-    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2){
 
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        Set<T> s1 = new HashSet<>(set1);
+        Set<T> s2 = new HashSet<>(set2);
+        s1.removeAll(s2);  // 3
+        Set<T> s11 = new HashSet<>(set1);
+        s2.removeAll(s11); // 0
+        s1.addAll(s2);     // 0, 3
+        return s1;
     }
 }
