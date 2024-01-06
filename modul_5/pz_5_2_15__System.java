94acc2572881dab8b0f0c724cda4bd37230c7c76
Sergo Fan <sergei.fonarev@yandex.ru>
2024-01-06 22:43:54 +0300

    add

--- modul_5/pz_5_2_15__System.java
+++ modul_5/pz_5_2_15__System.java
@@ -11,7 +11,10 @@ public class pz_5_2_15__System {
                 deque.addFirst(s.next());
             }
         }
-        deque.forEach(System.out::print);             // Ввод: 1 2 3 4 5 6 7 8 9 10
-                                                      // Вывод: 10 8 6 4 2
+      //  deque.forEach(System.out::print);                // Ввод: 1 2 3 4 5 6 7 8 9 10
+        int size = deque.size();
+        for (int i = 0; i < size; i++) {
+            System.out.print(deque.removeFirst() + " ");
+        }                                                    // Вывод: 10 8 6 4 2
     }
 }
