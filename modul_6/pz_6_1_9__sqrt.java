c74bd671bfa9d2c13256b3e7ac79aa1cca78a910
Sergo Fan <sergei.fonarev@yandex.ru>
2024-01-09 17:45:15 +0300

    add

--- modul_6/pz_6_1_9__sqrt.java
+++ modul_6/pz_6_1_9__sqrt.java
@@ -1,3 +1,17 @@
 public class pz_6_1_9__sqrt {
+    public interface UnaryOperator<T> {
+        T apply(T t);
+    }
 
+    public static class LambdaApp {
+        public UnaryOperator<Integer> sqrt() {
+            return x -> x * x;
+        }
+
+        public static void main(String[] args) {
+
+            UnaryOperator<Integer> square = x -> x*x;
+            System.out.println(square.apply(5)); // 25
+        }
+    }
 }
